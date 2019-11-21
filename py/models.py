6bed42559829a75272f0096e4681af061b9a0a71
Rene Quevedo <rquevedo@uhnresearch.ca>
2019-11-21 17:50:55 -0500

    Added deep ANN models to logistic regression

--- py/models.py
+++ py/models.py
@@ -6,8 +6,28 @@ class Models:
     def log_model(self):
         lr_model = Sequential()
         lr_model.add(Dense(1, input_shape=(X.shape[1],), activation='sigmoid'))
+        
         lr_model.compile(Adam(lr=0.01), 'binary_crossentropy', metrics=['accuracy'])
         self.model=lr_model
+        
+    def ann_model(self):
+        deep_model = Sequential()
+        deep_model.add(Dense(64, input_shape=(X.shape[1],), activation='tanh'))
+        deep_model.add(Dense(16, activation='tanh'))
+        deep_model.add(Dense(1, activation='sigmoid'))
+        
+        deep_model.compile(Adam(lr=0.01), 'binary_crossentropy', metrics=['accuracy'])
+        self.model=deep_model
+    
+    def ann_vis_model(self):
+        deep_model = Sequential()
+        deep_model.add(Dense(64, input_shape=(X.shape[1],), activation='tanh'))
+        deep_model.add(Dense(16, activation='tanh'))
+        deep_model.add(Dense(2, activation='tanh'))
+        deep_model.add(Dense(1, activation='sigmoid'))
+        
+        deep_model.compile(Adam(lr=0.01), 'binary_crossentropy', metrics=['accuracy'])
+        self.model=deep_model
 
 def get_model():
     return M.model
